4d2c5262aad3982e95a1e67582e54b6a70bbd6c1
Higor Diego <higordiegoti@gmail.com>
2018-03-17 19:27:03 -0300

    subindo

---
+++ _tester/authenticate.js
@@ -0,0 +1,41 @@
+describe('Routes Authorization', () => {
+    const User = require('../app/modules/models/user')
+    const rotaUrl = `${process.env.API_VERSION}/authenticate`
+    let defaultObject = objectAuthenticate
+
+    beforeEach((done) => {
+        Authorization.remove({})
+            .then(() => Authorization.create(defaultObject))
+            .then(response => {
+                defaultObject._id = `${response._id}`
+                done()
+            })
+            .catch(err => console.log('estou aqui', err))
+    })
+
+    describe(`Route get ${rotaUrl}`, (done) => {
+        it('should list a Authorization', (done) => {
+            requestAux.defaultGet(done, request, defaultObject, rotaUrl)
+        })
+    })
+
+    describe(`Route get ${rotaUrl}{_id}`, () => {
+        it('should return a one of Authorization', (done) => {
+            requestAux.defaultGetOne(done, request, defaultObject, `${rotaUrl}/${defaultObject._id}`)
+        })
+    })
+
+    describe(`Route put ${rotaUrl}{id}`, () => {
+        it('should update a Authorization', (done) => {
+            defaultObject.status = false
+            requestAux.defaultPut(done, request, defaultObject, `${rotaUrl}/${defaultObject._id}`)
+        })
+    })
+
+    describe(`Route delete ${rotaUrl}{id}`, () => {
+        it('should delete a Authorization', (done) => {
+            requestAux.defaultDelete(done, request, `${rotaUrl}/${defaultObject._id}`)
+        })
+    })
+
+})
